414091618ec376a28e251a91cfd7b1c1f08f053b
Bex Sander <rebeccajsander@me.com>
2023-05-19 10:58:43 +0100

    inital commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,71 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import * as d3 from 'd3';
 import './App.css';
 
+
 function App() {
+  const [geojson, setGeojson] = useState(null);
+  React.useEffect(() => {
+    if (geojson) {
+      return
+    }
+    fetch('/test.geojson')
+        .then((response) => response.json())
+        .then((geojson) => {
+          //  console.log("my_data: ", geojson);
+           setGeojson(geojson)
+
+        })
+
+}, [geojson, setGeojson])
+
+if (!geojson) {
+  return null
+}
+
+  // console.log(geojson)
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This is where my visualisation will go.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <Map geojson={geojson}/>
     </div>
+  );
+}
+
+function Map(props) { 
+  var map = React.useRef(null)
+  console.log(map)
+const geojson = props.geojson
+// console.log(geojson)
+let projection = d3.geoMercator()
+  // .scale(400)
+  // .translate([960, 500]);
+
+let geoGenerator = d3.geoPath()
+  .projection(projection);
+
+React.useEffect(() => {
+  let u = d3.select(map.current)
+    .selectAll('g')
+    .data(geojson.features);
+    // console.log(u)
+
+
+  u.enter()
+    .append('path')
+    .attr('d', geoGenerator);
+}, [map, geojson]);
+
+  return (
+    <div>
+    <svg ref={map} id="content" width="900px" height="500px">
+      <g className="map"></g>
+    </svg>
+  </div>
   );
 }
 

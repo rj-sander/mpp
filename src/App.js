83dcbd4726891fdcefc68bb49364d43fb43b8e50
Bex Sander <rebeccajsander@me.com>
2023-06-09 16:12:01 +0100

    transitions

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ function App() {
   const [clickIndex, setClickIndex] = useState(0);
 
 function Increase() {
-  if (clickIndex<4) {
+  document.getElementsByClassName("base")
+  if (clickIndex<5) {
     setClickIndex(clickIndex + 1);
   }
   else {
@@ -15,11 +16,12 @@ function Increase() {
 }
 
 function Decrease() {
+  document.getElementsByClassName("clicked")
   if (clickIndex>0) {
     setClickIndex(clickIndex - 1);
   }
   else {
-    setClickIndex(4);
+    setClickIndex(5);
   }
 }
 
@@ -35,15 +37,27 @@ Which Borough is spying on your Instagram?
         <caption>Scroll Down</caption>
       </header>
       <Testmap clickIndex={clickIndex} />
+
+<div className='container'>
+
+<div style={{display: `${clickIndex === 1 ? "inline": "none"}`}}>
+<div className='mapText'>
+Of 33 London boroughs contacted, 19 responded with answers about social media monitoring.</div></div>
+<div style={{display: `${clickIndex === 2 ? "inline": "none"}`}}><div className='mapText'>
+Of these, 13 said they conduct overt social media monitoring. This is defined differently by different councils but is usually accepted to be one-off searches, for example, looking at the first page of a social media profile.</div></div>
+<div style={{display: `${clickIndex === 3 ? "inline": "none"}`}}><div className='mapText'>
+Five boroughs admit to using covert social media monitoring, encompassing multiple searches across multiple platforms or repeated viewings over an extended period.</div></div>
+<div style={{display: `${clickIndex === 4 ? "inline": "none"}`}}>
+<div className='mapText'>Twelve boroughs say that they have an internal social media policy, including Westminster, despite not participating in surveillance. These policies are developed internally and vary wildly.</div></div>
+<div style={{display: `${clickIndex === 5 ? "inline": "none"}`}}>
+<div className='mapText'>No London Borough has reviewed or audited its policies or activity internally.</div></div>
+</div>
 <button onClick={Decrease}>isbutoon</button>
 <button onClick={Increase}>alsobutton</button>
-<p>Of 33 London boroughs contacted, 19 responded with answers about social media monitoring.
-Of these, 13 said they conduct overt social media monitoring. This is defined differently by different councils but is usually accepted to be one-off searches, for example, looking at the first page of a social media profile.
-Five boroughs admit to using covert social media monitoring, encompassing multiple searches across multiple platforms or repeated viewings over an extended period. 
-Twelve boroughs say that they have an internal social media policy, including Westminster, despite not participating in surveillance. These policies are developed internally and vary wildly. 
-No London Borough has reviewed or audited its policies or activity internally. 
-Barnet, Hackney, Islington, Kensington and Chelsea, and Redbridge use social media monitoring but have no policy available. This impacts almost 45,000 people living with disabilities across London who have no indication of whether their social media presence can impact their access to financial aid from their local council.
-Redbridge engages in overt and covert social media surveillance but has no guideline or policy on how this should be conducted. Over 9,000 people in Redbridge currently receive PIP.</p>
+<div>
+<p>Barnet, Hackney, Islington, Kensington and Chelsea, and Redbridge use social media monitoring but have no policy available. This impacts almost 45,000 people living with disabilities across London who have no indication of whether their social media presence can impact their access to financial aid from their local council.</p></div>
+<div>
+<p>Redbridge engages in overt and covert social media surveillance but has no guideline or policy on how this should be conducted. Over 9,000 people in Redbridge currently receive PIP.</p></div>
     </div>
   );
 }
